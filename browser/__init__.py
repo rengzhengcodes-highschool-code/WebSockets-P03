0cb3519351392647753a1c8835432943ed66e588
rengzhengcodes <rzhengcontacts@gmail.com>
2022-04-12 08:19:30 -0400

    boiler for template render

--- browser/__init__.py
+++ browser/__init__.py
@@ -1,1 +1,13 @@
-fro
+from flask import *
+
+app = Flask()
+
+@app.route('/')
+def main():
+	if 'name' not in session:
+		return render_template('index.html', name=None)
+	else:
+		return render_template('index.html', name=[session['name']])
+
+if __name__ == "__main__":
+	app.run(host="localhost", port=5000, debug=True)
