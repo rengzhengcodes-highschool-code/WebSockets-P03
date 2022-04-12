a070e9091354bdedd989060d40eeeba371a482b6
rengzhengcodes <rzhengcontacts@gmail.com>
2022-04-12 08:29:17 -0400

    boiler

---
+++ browser/static/socket.js
@@ -0,0 +1,19 @@
+//https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_client_applications
+
+var socket = new WebSocket('ws://127.0.0.1');
+
+function sendText() {
+	// makes a message object contianing the data needed to process the message
+	var msg = {
+		type: "message",
+		text: document.getElementById("input").value,
+		id: document.getElementById("user").value,
+		date: Date.now()
+	};
+
+	//send the msg as a JSON-formatted string.
+	socket.send(JSON.stringify(msg));
+
+	//blank the text input element
+	document.getElementById("text").value = "";
+}
